1aa2b8ad17f23f66ace16f378039c360bef50c2f
Paulo Keller <paulo.vinicius.keller@gmail.com>
2024-07-01 20:08:27 -0300

    create base recipt structure

---
+++ Assets/Scripts/Receipt/BulletReceipt.cs
@@ -0,0 +1,27 @@
+public class BulletReceipt : IReceipt
+{
+    public int MetalMaterialAmount 
+    {
+        get { return 5; }
+    }
+
+    public int ChemicalMaterialAmount 
+    {
+        get { return 0; }
+    }
+
+    public int EnergyMaterialAmount
+    {
+        get { return 0; }
+    }
+
+    public int FuelMaterialAmount
+    {
+        get { return 0; }
+    }
+
+    public int RadioactiveMaterialAmount
+    {
+        get { return 0; }
+    }
+}
---
+++ Assets/Scripts/Receipt/IReceipt.cs
@@ -0,0 +1,8 @@
+﻿public interface IReceipt
+{
+    int MetalMaterialAmount { get; }
+    int ChemicalMaterialAmount { get; }
+    int EnergyMaterialAmount { get; }
+    int FuelMaterialAmount { get; }
+    int RadioactiveMaterialAmount { get; }
+}
